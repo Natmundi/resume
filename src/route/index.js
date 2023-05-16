6095628633f4d886683560795bc21986b347225e
Natmundi <n.v.cherevata@outlook.com>
2023-05-16 19:08:49 +0200

    start brunch index8

--- src/route/index.js
+++ src/route/index.js
@@ -3809,6 +3809,16 @@ router.get('/shopcatalog', function (req, res) {
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
+
+router.get('/index8', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('index8', {
+    layout: 'index',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 module.exports = router
 
 //  master
